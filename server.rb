f1ff583752da1e68734b3034f318b620bfe8f3b8
Frank D <frankd412@gmail.com>
2021-10-28 15:48:31 +0000

    Switch from EM-MQTT to MQTT & save entries to database

--- server.rb
+++ server.rb
@@ -10,45 +10,38 @@ require './db.rb'
 EventMachine::error_handler { |e| puts "#{e}: #{e.backtrace.first}" }
 
 
-def messageCallback(msg)
+def messageCallback(msg, topic)
     #MQTT::Packet::Publish
     #bool flags[4]
     #topic
     #body_length
     #payload
-    puts YAML.dump(msg)
     topicRegex = Regexp.new("location/(?<device_location>[^/]+)/device/(?<device_name>[^/ ]+)")
     payloadRegex = Regexp.new("UPDATE.SENSOR (?<sensor_name>[^\s]+) (?<sensor_value>[^\s]+)")
-    payload =  payloadRegex.match(msg.payload)
-    topic =  topicRegex.match(msg.topic)
-    puts YAML.dump(msg.topic)
-    puts YAML.dump(msg.payload)
+    payload =  payloadRegex.match(msg)
+    topic =  topicRegex.match(topic)
+    if (!topic || !payload) then
+      return false
+    end
+    ap msg
+    ap topic
     deviceData = {
-      :sensor_name => payload.sensor_name,
+      :sensor_name => payload[:sensor_name],
       :sensor_hardware => "changeme",
-      :sensor_value => payload.sensor_value,
-      :device_location => topic.device_location,
-      :device_name => topic.device_name
+      :sensor_value => payload[:sensor_value],
+      :device_location => topic[:device_location],
+      :device_name => topic[:device_name]
     }
     d = DeviceDatum.new(deviceData)
-    #d.save!
+    d.save!
     ap deviceData
     ap d
 end
 
-EventMachine.run do
-    EventMachine::MQTT::ClientConnection.connect(
-    :host => '192.168.0.133',
-    :username => 'emqx',
-    :password => 'public'
-  ) do |c|
-    c.subscribe('#')
-    c.receive_callback do |message|
-      h = { :topic => message.topic, :msg => message.payload }
-      ap h
-      p message
-      messageCallback(message)
-    end
+topicRegex = Regexp.new("location/(?<device_location>[^/]+)/device/(?<device_name>[^/ ]+)")
+payloadRegex = Regexp.new("UPDATE.SENSOR (?<sensor_name>[^\s]+) (?<sensor_value>[^\s]+)")
+MQTT::Client.connect(:host => '192.168.0.133', :username => 'emqx',:password => 'public') do |c|
+  c.get('#') do |topic, message|
+    messageCallback(message, topic)
   end
 end
-
